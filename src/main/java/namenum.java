aba867321d2e46e9a17458f4d7b8b902aed30eaf
Ben Zhao <benjakezhao@gmail.com>
2021-06-20 16:02:15 -0700

    first final edition

--- src/main/java/namenum.java
+++ src/main/java/namenum.java
@@ -1,15 +1,36 @@
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class namenum {
-    public static void main(String[] args) throws FileNotFoundException {
+
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(new File("namenum.in"));
         String cowId = scanner.nextLine();
 
         List<String> words = Files.readAllLines(Path.of("namenumdict.txt"));
+        List<String> result = findMatched(cowId, words);
+        File file = new File("namenum.out");
+        PrintWriter out = new PrintWriter("namenum.out");
+        if(result.isEmpty()) {
+            out.println("None");
+        }
+        for(String s : result) {
+            out.println(s);
+        }
+        out.flush();
+    }
+    private static char[] MAPPINGS = {
+            '2', '2', '2', '3', '3', '3','4', '4', '4','5', '5', '5',
+            '6', '6', '6', '7', '0', '7', '7','8', '8', '8','9', '9', '9', '0'
+    };
+    private static List<String> findMatched(String cowId, List<String> words) {
         List<String> result = new ArrayList<>();
-        char[] chars = cawId.toCharArray();
+        char[] chars = cowId.toCharArray();
         for (String word : words) {
             if (word.length() != cowId.length()) {
                 continue;
@@ -17,24 +38,7 @@ public class namenum {
             boolean matched = true;
             for (int i = 0; i < word.length(); i++) {
                 char c = word.charAt(i);
-                char converted = '0';
-                if(c == 'A' || c == 'B' || c == 'C') {
-                    converted = '2';
-                }else if (c == 'D' || c == 'E' || c == 'F') {
-                    converted = '3';
-                }else if (c == 'G' || c == 'H' || c == 'I') {
-                    converted = '4';
-                }else if (c == 'J' || c == 'K' || c == 'L') {
-                    converted = '5';
-                }else if (c == 'M' || c == 'N' || c == 'O') {
-                    converted = '6';
-                }else if (c == 'P' || c == 'R' || c == 'S') {
-                    converted = '7';
-                }else if (c == 'T' || c == 'U' || c == 'V') {
-                    converted = '8';
-                }else if (c == 'W' || c == 'X' || c == 'Y') {
-                    converted = '9';
-                }
+                char converted = MAPPINGS[c - 'A'];
                 if (chars[i] != converted) {
                     matched = false;
                     break;
@@ -44,10 +48,6 @@ public class namenum {
                 result.add(word);
             }
         }
-        File file = new File("namenum.out");
-        PrintWriter out = new PrintWriter("namenum.out");
-        for(String s : result) {
-            out.println(s);
-        }
+        return result;
     }
 }
